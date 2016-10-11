2d45b0cf30a127821b32e4f6bee041922afd5940
Johan Jordaan <johan@inmydiary.com>
2016-10-11 11:10:02 +1100

    Fixed the camera reopen issues

--- index.ios.js
+++ index.ios.js
@@ -1,87 +1,108 @@
 'use strict';
 import React, { Component } from 'react';
 import {
-  AppRegistry,
-  Dimensions,
-  StyleSheet,
-  Text,
-  TouchableHighlight,
-  View
+   AppRegistry,
+   Dimensions,
+   StyleSheet,
+   Text,
+   TouchableHighlight,
+   View
 } from 'react-native';
 import Camera from 'react-native-camera';
 
+const CAMERA = 'camera';
+const AFTER = 'after';
+
 class LikeStuffApp extends Component {
    constructor () {
-		super();
+	   super();
       this.watchID = null;
 		this.state = {
+         currentScreen: CAMERA,
          lastPosition: 'unknown',
 		};
 	}
 
    componentDidMount() {
-      navigator.geolocation.getCurrentPosition(
-        (position) => {
+      navigator.geolocation.getCurrentPosition((position) => {
           var initialPosition = JSON.stringify(position);
           this.setState({initialPosition});
         },
-        (error) => alert(error.message),
-        {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
+       (error) => alert(error.message),
+       {enableHighAccuracy: true, timeout: 20000, maximumAge: 1000}
       );
       this.watchID = navigator.geolocation.watchPosition((position) => {
-        var lastPosition = JSON.stringify(position);
-        this.setState({lastPosition:lastPosition});
+         var lastPosition = JSON.stringify(position);
+         this.setState({lastPosition:lastPosition});
       });
-    }
+   }
 
-    componentWillUnmount() {
+   componentWillUnmount() {
       navigator.geolocation.clearWatch(this.watchID);
-    }
+   }
 
+   renderCamera() {
+      return (
+        <View style={styles.container}>
+         <Camera
+            ref="camera"
+            style={styles.preview}
+            aspect={Camera.constants.Aspect.fill}
+            captureAudio={false}>
+            <Text>
+            <Text style={styles.title}>Initial position: </Text>
+            {this.state.initialPosition}
+         </Text>
+            <Text style={styles.capture} onPress={this.lovePicture.bind(this)}>[Love]</Text>
+            <Text style={styles.capture} onPress={this.hatePicture.bind(this)}>[Hate]</Text>
+         </Camera>
+        </View>
+      );
+   }
+   renderAfter() {
+      return (
+         <View style={styles.container}>
+            <Text style={styles.capture} onPress={this.doAfter.bind(this)}>[Back]</Text>
+         </View>
+      );
+   }
 
-  render() {
-    return (
-      <View style={styles.container}>
-        <Camera
-          ref={(cam) => {
-            this.camera = cam;
-          }}
-          style={styles.preview}
-          aspect={Camera.constants.Aspect.fill}
-          captureAudio={false}>
-          <Text>
-          <Text style={styles.title}>Initial position: </Text>
-          {this.state.initialPosition}
-        </Text>
-          <Text style={styles.capture} onPress={this.lovePicture.bind(this)}>[Love]</Text>
-          <Text style={styles.capture} onPress={this.hatePicture.bind(this)}>[Hate]</Text>
-        </Camera>
-      </View>
-    );
-  }
+   render() {
+      if(this.state.currentScreen === CAMERA) {
+         return this.renderCamera();
+      } else {
+         return this.renderAfter();
+      }
+   }
 
-  lovePicture() {
-     var location = ""; //TODO : get location
-     this.handlePicture("love",location).then(()=>{
+   doAfter() {
+      this.setState({currentScreen:CAMERA});
 
-     });
- }
+   }
 
-  hatePicture() {
-     var location = ""; //TODO : get location
-     this.handlePicture("hate",location).then(()=>{
-     });
-  }
+   lovePicture() {
+      var location = ""; //TODO : get location
+      this.handlePicture("love",location).then(()=>{
+      });
+   }
 
-  handlePicture(caption,location) {
-     return this.camera.capture().then((data)=>{
-       // Send caption - with picture to server
-       // Remove from your camera roll?
-       // Send the picture to the server for processing
-     }).catch((err)=>{
+   hatePicture() {
+      var location = ""; //TODO : get location
+      this.handlePicture("hate",location).then(()=>{
+      });
+   }
+
+   handlePicture(caption,location) {
+      return this.refs.camera.capture().then((data)=>{
+         this.setState({currentScreen:AFTER})
+         // Send caption - with picture to server
+         // Remove from your camera roll?
+            // Send the picture to the server for processing
+      }).catch((err)=>{
+         console.log(err);
        // TODO : Do something with the error
-     })
-  }
+      })
+   }
 }
 
 const styles = StyleSheet.create({
